0417ac5ef782126411444b9963f2e020878e37a0
UgnilJoZ <janolezab@aol.com>
2015-08-15 00:00:26 +0200

    Added rudimentary mountains

---
+++ alphamg_mountains/init.lua
@@ -0,0 +1,37 @@
+alphamg.np_mountains = {
+	offset = 0,
+	scale = 1,
+	spread = {x=256, y=256, z=256},
+	octaves = 2,
+	seeddiff = 42696,
+	persist = 0.7
+}
+-- turns a common noise into a "web"-shaped noise
+local function web_noise(noiseval)
+	local x = 1-3*math.abs(noiseval)
+	if x < 0 then
+		return 0
+	else
+		return 0.5 + math.sin(math.pi * (x - 0.5)) / 2
+	end
+end
+function alphamg.add_mountains(heightmap, minp,maxp)
+	if alphamg.verbose then
+		print("[alphamg.add_river]")
+	end
+	local hm = heightmap
+	local chulens = {x=maxp.x-minp.x+1, y=maxp.y-minp.y+1, z=maxp.z-minp.z+1}
+	local mountain_noise = minetest.get_perlin_map(alphamg.np_mountains, chulens):get2dMap_flat({x=minp.x, y=minp.z, z=minp.z})
+
+	local nixz = 1
+	for z = minp.z,maxp.z do
+	for x = minp.x,maxp.x do
+		if hm[nixz] > 0 then
+			hm[nixz] = (1 + web_noise(mountain_noise[nixz])*3) * hm[nixz]
+		end
+		nixz = nixz + 1
+	end
+	end
+	return hm
+end
+alphamg.add_heightmap_generation_handler(alphamg.add_mountains)
